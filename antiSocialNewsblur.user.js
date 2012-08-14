abd467fdb994af2cb81a822dfa432248585785f3
Eskild Hustvedt <code@zerodogg.org>
2012-08-14 09:56:34 +0200

    Added license

--- antiSocialNewsblur.user.js
+++ antiSocialNewsblur.user.js
@@ -6,6 +6,21 @@
 // @description    Remove the social aspects of Newsblur
 // ==/UserScript==
 
+// Copyright (C) Eskild Hustvedt 2012
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 var style = document.createElement('style');
 style.innerHTML = ''+
 /* Remove this line if you like the new sidebar with "train" and "save" */
